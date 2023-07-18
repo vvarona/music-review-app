38a44d4fae1afa28f715f1ef851c0061db13830f
vvarona <vvaronad@gmail.com>
2023-07-18 21:33:53 +0200

    feat: search bar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import styles from "../assets/styles/SearchBar.module.css"
 
 export default function SearchBar(prop) {
   return (
-    <form onSubmit={prop.handleSubmit}>
-      <label htmlFor="albumSearch">Search for an album</label>
+    <form className={styles.container} onSubmit={prop.handleSubmit}>
+      <label 
+      className={styles.label}
+      htmlFor="albumSearch">Search for an album</label>
       <input
+        className={styles.input}
         type="text"
         name="albumSearch"
         id="albumSearch"
