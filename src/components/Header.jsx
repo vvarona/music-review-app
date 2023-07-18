7e835ff9ffff25ae750a3b0fb1c4925091373f66
vvarona <vvaronad@gmail.com>
2023-07-18 21:32:52 +0200

    feat: header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "../assets/styles/Header.module.css"
+
 
 export default function Header() {
   return (
-    <header className="header">
+    <header className={styles.header}>
       <nav>
-        <ul>
+        <ul className={styles.list}>
             <li><NavLink to="/" >Home</NavLink></li>
             <li><NavLink to="/my-reviews">My reviews</NavLink></li>
             <li><NavLink to="/album-page">Album page</NavLink></li>
