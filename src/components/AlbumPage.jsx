2cfae3b97017a38b7ff506ea35e8816477f647c1
Victoria Varona <vvaronad@gmail.com>
2023-07-28 20:29:02 +0200

    chore: update list component

--- src/components/AlbumPage.jsx
+++ src/components/AlbumPage.jsx
@@ -3,12 +3,12 @@ import { useParams } from 'react-router-dom'
 
 export default function AlbumPage(props) {
     const { albumID } = useParams() 
-    const album = props.albums.find(album.id === albumID)
+    
   return (
-    <div>
-      <img src={`${album.cover}`} alt={`Cover of ${album.album_title}`}  />
-      <h2>{album.album_title}</h2>
-      <h3>by {artist}</h3>
-    </div>
+    <section>
+     
+      <h2>{albumID}</h2>
+
+    </section>
   )
 }
