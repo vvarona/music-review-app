4d5da2334f1dcb443ce667e623749075e4b0d424
Victoria Varona <vvaronad@gmail.com>
2023-07-31 22:22:54 +0200

    chore: Album page data fetching

--- src/components/AlbumPage.jsx
+++ src/components/AlbumPage.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { mockAlbum as album } from "../assets/AlbumModel";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function AlbumPage(props) {
-    const { albumID } = useParams() 
-    
+  const { albumID } = useParams();
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios
+      .get("http://localhost:3003/album/" + albumID)
+      .then((response) => setData(response.data.album))
+  }, [albumID]);
+
   return (
     <section>
-     
-      <h2>{albumID}</h2>
-
+      
     </section>
-  )
+  );
 }
